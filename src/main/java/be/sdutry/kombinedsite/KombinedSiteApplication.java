3ccf629af27ac023987562725888178f29044b8c
Stefaan Dutry <stefaan.dutry@gmail.com>
2020-03-25 10:05:09 +0100

    #1 creation of project

---
+++ src/main/java/be/sdutry/kombinedsite/KombinedSiteApplication.java
@@ -0,0 +1,13 @@
+package be.sdutry.kombinedsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KombinedSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KombinedSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/sdutry/kombinedsite/KombinedSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package be.sdutry.kombinedsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KombinedSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
