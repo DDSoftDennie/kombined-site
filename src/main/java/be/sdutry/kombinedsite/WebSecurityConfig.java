223db7cbe0904677d902d3e09cac5ce24e9b272d
Stefaan Dutry <stefaan.dutry@gmail.com>
2020-04-01 23:33:08 +0200

    initial adding of security

--- src/main/java/be/sdutry/kombinedsite/WebSecurityConfig.java
+++ src/main/java/be/sdutry/kombinedsite/WebSecurityConfig.java
@@ -4,13 +4,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/matching", "/user/register", "/user/login").permitAll().anyRequest()
-				.authenticated().and().formLogin().loginPage("/user/login").permitAll().and().logout().permitAll();
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/user/register").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/user/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails admin = User.withDefaultPasswordEncoder().username("admin@kombined.be").password("admin").roles("USER", "ADMIN").build();
+		UserDetails test = User.withDefaultPasswordEncoder().username("test@kombined.be").password("test").roles("USER").build();
+		
+		return new InMemoryUserDetailsManager(admin, test);
 	}
 }
